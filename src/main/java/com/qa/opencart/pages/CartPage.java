5bd662cd3765fcc7c67e33ae78f237e8a32e5474
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2024-05-21 22:56:30 +0530

    added cart page

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("my cart page");
+		// TODO Auto-generated method stub
+
+	}
+
+}
